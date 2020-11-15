50f697ac6e532aa7512af7159b5dc91cb0491128
Patrik Marin <patrik.marin@qoco.fi>
2020-11-15 13:58:38 +0200

    Style ratings

--- src/features/Ratings/RatingBlock.tsx
+++ src/features/Ratings/RatingBlock.tsx
@@ -2,16 +2,48 @@ import React from 'react';
 
 import styles from './RatingBlock.module.scss';
 
-const RatingBlock: React.FC<{}> = () => {
+export interface CoffeeRating {
+  manufacturer: string;
+  rating: number;
+  name: string;
+  bagPrice: number;
+  bagWeight: number;
+  notes: string[];
+  date: string;
+  description: string;
+}
+
+const RatingBlock: React.FC<CoffeeRating> = ({
+  manufacturer,
+  rating,
+  name,
+  bagPrice,
+  bagWeight,
+  notes,
+  date,
+  description,
+}) => {
+  const stars = [...new Array(10)]
+    .map((val, ind) => (rating > ind ? '★' : '☆'))
+    .join(' ');
+
+  const price = String((bagPrice / bagWeight).toFixed(2));
+
   return (
-    <div className={styles.rating}>
-      <div className={styles.img} />
-      <div className={styles.manufacturer}>Nordqvist</div>
-      <div className={styles.name}>Jubileum</div>
-      <div className={styles.price}>1.00€ / 100g</div>
-      <div className={styles.notes}>fruity</div>
-      <div className={styles.date}>2020-06-06</div>
-      <div className={styles.description}>Oikein hyvä.</div>
+    <div
+      className={styles['rating-block']}
+      key={`${date}-${manufacturer}-${name}`}
+    >
+      <div className={styles.img}></div>
+      <div className={styles.manufacturer}>{manufacturer}</div>
+      <div className={styles.rating}>{stars}</div>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.price}>{price}€ / 100g</div>
+      <div className={styles.notes}>{notes}</div>
+      <div className={styles.date}>{date}</div>
+      <div className={styles.description}>
+        <i>{description}</i>
+      </div>
     </div>
   );
 };
--- src/features/Ratings/index.tsx
+++ src/features/Ratings/index.tsx
@@ -3,12 +3,35 @@ import styles from './Ratings.module.scss';
 
 import {Button} from '../../components/Button';
 
-import {RatingBlock} from './RatingBlock';
+import {RatingBlock, CoffeeRating} from './RatingBlock';
+
+const coffeeRatings: CoffeeRating[] = [
+  {
+    manufacturer: 'Nordqvist',
+    rating: 5,
+    name: 'Jubileum',
+    bagPrice: 339,
+    bagWeight: 500,
+    notes: ['fruity'],
+    date: '2020-06-06',
+    description: 'Hyvä.',
+  },
+  {
+    manufacturer: 'Nordqvist',
+    rating: 5,
+    name: 'Jubileum',
+    bagPrice: 339,
+    bagWeight: 500,
+    notes: ['fruity'],
+    date: '2020-06-06',
+    description: 'Hyvä.',
+  },
+];
 
 const Ratings: React.FC<{}> = () => {
   return (
     <div>
-      <RatingBlock />
+      <div className={styles['ratings']}>{coffeeRatings.map(RatingBlock)}</div>
       <div className={styles['new-rating']}>
         <Button>Add new rating</Button>
       </div>
